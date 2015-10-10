b712facd2501d8263706cd1c7b86c798c8202735
nikitasonthalia <89679.sonthalia@students.itu.edu>
2015-10-09 17:02:35 -0700

    This is Onetime trip registration  module

--- src/main/java/com/itu/shareonwheels/controller/LoginController.java
+++ src/main/java/com/itu/shareonwheels/controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.itu.shareonwheels.controller;
 
+import com.itu.shareonwheels.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by nikitasonthalia on 10/9/15.
  */
 public class LoginController {
+
+    @Autowired
+    User user ;
 
 }
