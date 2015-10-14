55e9d708a1ea73c052a1e165289f2587693a6e97
ramya.dinesh <ramya.dinesh@yesmeapp.com>
2015-10-13 21:55:03 -0700

    adding spring boot

---
+++ src/main/java/com/itu/shareonwheels/controller/TripController.java
@@ -0,0 +1,14 @@
+package com.itu.shareonwheels.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by ramya on 10/10/15.
+ */
+@Controller
+public class TripController {
+
+
+
+
+}
--- src/main/java/com/itu/shareonwheels/launcher/ShareOnWheelsServer.java
+++ src/main/java/com/itu/shareonwheels/launcher/ShareOnWheelsServer.java
@@ -3,12 +3,34 @@ package com.itu.shareonwheels.launcher;
 /**
  * Created by ramya on 10/2/15.
  */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
 public class ShareOnWheelsServer {
 
-    public static void main(String[] args) throws Exception
+
+    @Bean
+    public DriverManagerDataSource dataSource()
     {
-//        Server server = new Server(8080);
-//        server.start();
-//        server.join();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/shareonwheels");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+        try {
+            dataSource.getConnection().setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareOnWheelsServer.class, args);
     }
 }
